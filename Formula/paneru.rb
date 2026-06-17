e35231c5444d8263d4ea2792f2e097c204dd9e87
karinushka <barbarush@proton.me>
2026-06-17 21:35:12 +0200

    Bump the tgz version to 0.4.3.

--- Formula/paneru.rb
+++ Formula/paneru.rb
@@ -1,7 +1,7 @@
 class Paneru < Formula
   desc "Sliding, tiling window manager for MacOS"
   homepage "https://github.com/karinushka/paneru"
-  url "https://github.com/karinushka/paneru/archive/refs/tags/v0.4.2.tar.gz"
+  url "https://github.com/karinushka/paneru/archive/refs/tags/v0.4.3.tar.gz"
   sha256 "5d6bb92c549a1e608e2d54a44dd828651ab29cc0e51f948bf94b14bdc1781c70"
   license "MIT"
   head "https://github.com/karinushka/paneru.git", branch: "main"
